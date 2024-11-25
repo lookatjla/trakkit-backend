f4f6f228c8d06ee2f1d05b2a3f0fdeaf838b9e3e
Jayla Young <jaylayoung@Jaylas-MacBook-Pro-2.local>
2024-11-25 14:46:00 -0500

    add existing project files to git

---
+++ src/main/java/dev/jayla/stockTrendTracker/StockTrendTrackerApplication.java
@@ -0,0 +1,13 @@
+package dev.jayla.stockTrendTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockTrendTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockTrendTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/jayla/stockTrendTracker/StockTrendTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.jayla.stockTrendTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockTrendTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
